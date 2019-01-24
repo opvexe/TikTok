0cc43811b2c6734f60a337927adba01839ce1a95
FaceBook <tao_shuminX@foxmail.com>
2019-01-24 15:09:07 +0800

    no message

--- TikTok/TikTok/Pages/Home/TTHomeViewController.m
+++ TikTok/TikTok/Pages/Home/TTHomeViewController.m
@@ -34,19 +34,9 @@ UIViewControllerTransitioningDelegate
     [self setStatusBarHidden:NO];
     [self.navigationController setNavigationBarHidden:NO animated:animated];
 }
-//
-//- (void)viewWillAppear:(BOOL)animated {
-//    [super viewWillAppear:animated];
-//    //    [self setNavigationBarTitleColor:[UIColor clearColor]];
-//    //    [self setNavigationBarBackgroundColor:[UIColor clearColor]];
-//    //    [self setStatusBarBackgroundColor:[UIColor clearColor]];
-//    //    [self setNavigationBarBackgroundImage:[UIImage imageWithColor:[UIColor clearColor]]];
-//    
-//}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //    [self settingNativeItemsView];
     [self configView];
     [self refreshLoadDataSoucre];
     if (iOS11) {
@@ -90,60 +80,6 @@ UIViewControllerTransitioningDelegate
     self.lists = [TTAwemeModel mj_objectArrayWithKeyValuesArray:awemes[@"data"]];
     [self.tableListView reloadData];
 }
-
-//-(void)settingNativeItemsView{
-//
-//    _segment = ({
-//        UISegmentedControl *iv = [[UISegmentedControl alloc]initWithItems:@[@"推荐",@"北京"]];
-//        iv.frame = CGRectMake(0, 0, Number(60.0f), NumberHeight(30));
-//        iv.tintColor = [UIColor clearColor];
-//        iv.selectedSegmentIndex = 0;
-//        NSDictionary * normalTextAttributes = @{NSFontAttributeName : [UIFont SYPingFangSCSemiboldFontOfSize:16.0f],NSForegroundColorAttributeName : [UIColor TextGrayColor]};
-//        [iv setTitleTextAttributes:normalTextAttributes forState:UIControlStateNormal];
-//        NSDictionary * selctedTextAttributes = @{NSFontAttributeName : [UIFont SYPingFangSCSemiboldFontOfSize:18.0f],NSForegroundColorAttributeName : [UIColor whiteColor]};
-//        [iv setTitleTextAttributes:selctedTextAttributes forState:UIControlStateSelected];
-//        [iv addTarget:self action:@selector(segmentAction:) forControlEvents:UIControlEventValueChanged];
-//        self.navigationItem.titleView = iv;
-//        iv;
-//    });
-//
-//    UIButton *leftView = [UIButton buttonWithType:UIButtonTypeCustom];
-//    leftView.frame = CGRectMake(0, 0, Number(50.0f), NumberHeight(30.0f));
-//    [leftView setImage:[UIImage imageNamed:@"iconStoryhomeCamera"] forState:UIControlStateNormal];
-//    [leftView setImage:[UIImage imageNamed:@"iconStoryhomeCamera"] forState:UIControlStateHighlighted];
-//    [leftView setImage:[UIImage imageNamed:@"iconStoryhomeCamera"] forState:UIControlStateSelected];
-//    [leftView setImage:[UIImage imageNamed:@"iconStoryhomeCamera"] forState:UIControlStateDisabled];
-//    [leftView setTitle:@"随拍" forState:UIControlStateNormal];
-//    [leftView setTitle:@"随拍" forState:UIControlStateHighlighted];
-//    [leftView setTitle:@"随拍" forState:UIControlStateSelected];
-//    [leftView setTitle:@"随拍" forState:UIControlStateDisabled];
-//    [leftView setTitleColor:[UIColor TextGrayColor] forState:UIControlStateNormal];
-//    [leftView setTitleColor:[UIColor TextGrayColor] forState:UIControlStateHighlighted];
-//    [leftView setTitleColor:[UIColor TextGrayColor] forState:UIControlStateSelected];
-//    [leftView setTitleColor:[UIColor TextGrayColor] forState:UIControlStateDisabled];
-//    [leftView.titleLabel setFont:[UIFont SYHelveticaFontOfSize:14.0f]];
-//    [leftView layoutTextWithImageButtonStyle:layoutTextRightImageButton withSpace:3.0f];
-//    self.navigationleftView = leftView;
-//
-//    TTRightItemView *rightView = [[TTRightItemView alloc]initWithFrame:CGRectMake(0, 0, Number(80.0f), NumberHeight(30.0f))];
-//    self.navigationRightView = rightView;
-//    [rightView compelet:^(TTRightItemViewClickType type) {
-//        switch (type) {
-//            case TTRightItemViewClickTypeSearch:{
-//
-//            }
-//                break;
-//            case TTRightItemViewClickTypeLiving:{
-//
-//            }
-//                break;
-//            default:
-//                break;
-//        }
-//    }];
-//}
-
-
 
 #pragma mark - UITableViewDelegate && UITableViewDataSource
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
